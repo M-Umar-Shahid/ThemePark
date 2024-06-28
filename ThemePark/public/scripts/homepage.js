c8b9eef699dc5617783d8b630b5106bc56004996
Muhammad Umar Shahid <159911362+M-Umar-Shahid@users.noreply.github.com>
2024-06-28 07:05:59 +0500

    Navbar and Sidebar

--- ThemePark/public/scripts/homepage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  const header = $("header");
-
-  $(window).scroll(function () {
-    header.toggleClass("sticky", $(this).scrollTop() > 0);
-  });
-});
--- ThemePark/public/scripts/sidebar.js
+++ ThemePark/public/scripts/sidebar.js
@@ -1,59 +1,26 @@
-$(document).ready(function () {
-  window.openSidebar = function () {
-    $("#sidebar").css("width", "250px"); // Adjust width as needed
-    showCart();
-  };
-
-  window.closeSidebar = function () {
-    $("#sidebar").css("width", "0");
-  };
-
-  // Toggle sidebar when clicking cart icon
-  $(".cart-icon").click(function () {
-    openSidebar();
+window.onload = function () {
+  document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("sidebar").classList.add("open");
   });
 
-  function calculateGrandTotal(cart) {
-    let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-      total += parseFloat(cart[i].price);
-    }
-    return total;
-  }
+  document.getElementById("close-btn").addEventListener("click", function () {
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sub-menu").classList.remove("open");
+  });
 
-  function showCart() {
-    var cart = JSON.parse(Cookies.get("cart") || "[]");
-    var cartItemsHtml = cart
-      .map(
-        (item) => `
-      <li class="cart-item" data-id="${item.id}">
-        <p>Name: ${item.name}</p>
-        <p>Price: Rs.${item.price}</p>
-        <p>Quantity: ${item.quantity}</p>
-        <button class="delete-item" data-id="${item.id}" style="background-color: red; color: white; border: none; padding: 5px 10px; cursor: pointer;">Delete</button>
-      </li>
-    `
-      )
-      .join("");
+  const parkAndRide = document.getElementById("parks-rides-label");
+  parkAndRide.addEventListener("mouseover", function () {
+    document.getElementById("sub-menu").style.display = "block";
+    document.getElementById("sub-menu").classList.add("open");
+  });
 
-    var grandTotal = calculateGrandTotal(cart);
-    $("#grand-total").text(`Grand Total: Rs. ${parseInt(grandTotal)}`);
-
-    $("#cart-items").html(cartItemsHtml);
-
-    // Attach event listener for delete buttons
-    $(".delete-item").click(function () {
-      var perfumeId = $(this).data("id");
-      var cart = JSON.parse(Cookies.get("cart") || "[]");
-
-      // Remove the item from the cart
-      cart = cart.filter((item) => item.id !== perfumeId);
-
-      // Save the updated cart to cookies
-      Cookies.set("cart", JSON.stringify(cart), { expires: 7 });
-
-      // Update the cart items list in the UI
-      showCart();
+  document
+    .getElementById("sub-menu")
+    .addEventListener("mouseleave", function () {
+      document.getElementById("sub-menu").classList.remove("open");
     });
-  }
-});
+
+  document.getElementById("events").addEventListener("mouseover", function () {
+    document.getElementById("sub-menu").classList.remove("open");
+  });
+};
