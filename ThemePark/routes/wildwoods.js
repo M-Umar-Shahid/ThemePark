606397caf65755d0da3cbfe8d020469ffc655858
Muhammad Umar Shahid <159911362+M-Umar-Shahid@users.noreply.github.com>
2024-06-29 23:36:27 +0500

    sealiferoute

--- ThemePark/routes/wildwoods.js
+++ ThemePark/routes/wildwoods.js
@@ -1,15 +1,16 @@
 let express = require("express");
 let router = express.Router();
-const db = require("../connection"); 
-
+const db = require("../connection");
 
 router.get("/", (req, res) => {
   res.render("WildWoodsArea");
 });
 
 router.get("/dreamworks-water-park", (req, res) => {
-    res.render("WaterParkRide");
-  });
-  
+  res.render("WaterParkRide");
+});
+router.get("/sea-life", (req, res) => {
+  res.render("SeaLifeRide");
+});
 
 module.exports = router;
