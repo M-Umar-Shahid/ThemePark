175f78b5dd2afd83fcb4bd87e4ef4b3268065577
M Bilal Naeem <bn.4389@gmail.com>
2024-07-03 04:55:31 +0500

    w3c validations done

--- ThemePark/routes/homepage.js
+++ ThemePark/routes/homepage.js
@@ -3,12 +3,12 @@ let router = express.Router();
 const db = require("../db_connection");
 
 router.get("/", (req, res) => {
-  db.all("SELECT * FROM areas LIMIT 3", [], (err, areas) => {
+  db.all("SELECT * FROM areas ORDER BY RANDOM() LIMIT 3", [], (err, areas) => {
     if (err) {
       console.error("Error fetching areas:", err);
       res.status(500).send("Internal Server Error");
     } else {
-      db.all("SELECT * FROM rides LIMIT 3", [], (err, rides) => {
+      db.all("SELECT * FROM rides ORDER BY RANDOM() LIMIT 3", [], (err, rides) => {
         if (err) {
           console.error("Error fetching rides:", err);
           res.status(500).send("Internal Server Error");
